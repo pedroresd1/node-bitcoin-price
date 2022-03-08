b0919df6d88e27a6e9cd309f5705bc2b41d5d900
blzpredo <pedroresd1@gmail.com>
2022-03-08 16:35:05 -0300

    encapsulamento

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const request = require('request');
 
-const getPriceCoinBase = () => {
-    const url = 'http://api.coinbase.com/v2/prices/spot?currency=USD';
-
+const doRequest = (url) => {
     const callback = (error, httpResponse, body) => {
         const response = JSON.parse(body);
-        console.log(response);
+        console.log(response)
     };
     request(url, callback);
 };
+
+const getPriceCoinBase = () => {
+    const url = 'http://api.coinbase.com/v2/prices/spot?currency=USD';
+
+    doRequest(url);
+};
+
 getPriceCoinBase();
