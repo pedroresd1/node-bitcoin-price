add01b43094b3a71794fd13e0ea2a7cc0bb0c3a1
blzpredo <pedroresd1@gmail.com>
2022-03-08 16:43:14 -0300

    retornando moeda e o preço atual

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
-const doRequest = (url) => {
+const doRequest = (url, dataPrice, dataCurrency) => {
     const callback = (error, httpResponse, body) => {
         const response = JSON.parse(body);
-        console.log(response)
+        const price = dataPrice(response);
+        const currency = dataCurrency(response);
+        console.log(currency + ' ' + price);
     };
     request(url, callback);
 };
 
 const getPriceCoinBase = () => {
     const url = 'http://api.coinbase.com/v2/prices/spot?currency=USD';
-
-    doRequest(url);
+    const dataPrice = (response) => response.data.amount;
+    const dataCurrency = (response) => response.data.currency;
+    doRequest(url,dataPrice,dataCurrency);
 };
 
 getPriceCoinBase();
